41cc07de8b575728647095697b5242ef3395e4f1
Test-codes-03 <ksudarshanksudarshan@gmail.com>
2025-04-19 21:37:35 +0530

    Update clientSchema.js

--- models/clientSchema.js
+++ models/clientSchema.js
@@ -1,44 +1,48 @@
-const mongoose = require('mongoose');
 const ClientSchema = mongoose.Schema({
-    userid:{
-        type: String,
-        required: [true, 'Please enter user id'],
+    userid: {
+      type: String,
+      required: [true, 'Please enter user id'],
     },
-    name:{
-        type: String,
-        required: [true, 'Please enter proper name'],
+    name: {
+      type: String,
+      required: [true, 'Please enter proper name'],
     },
-    amount:{
-        type: String,
-        required: [true, 'Please enter amount'],
+    amount: {
+      type: String,
+      required: [true, 'Please enter amount'],
     },
-    email:{
-        type: String,
-        required: [true, 'Please enter email'],
+    email: {
+      type: String,
+      required: [true, 'Please enter email'],
     },
-    phone_number:{
-        type: String,
-        required: [true, 'Please enter phone number'],
+    phone_number: {
+      type: String,
+      required: [true, 'Please enter phone number'],
     },
-    select_type:{
-        type: String,
-        required: [true, 'Please enter type'],
+    select_type: {
+      type: String,
+      required: [true, 'Please enter type'],
     },
-     schemes:{
-        type: String,
-        required: [true, 'Please enter schemes'],
+    schemes: {
+      type: String,
+      required: [true, 'Please enter schemes'],
     },
-    paymentId:{
-        type: String,
-        required: [true, 'Please enter paymentId'],
+    paymentId: {
+      type: String,
+      required: [true, 'Please enter paymentId'],
     },
-    paymentStatus:{
-        type: String,
-        required: [true, 'Please enter paymentStatus'],
+    paymentStatus: {
+      type: String,
+      required: [true, 'Please enter paymentStatus'],
     },
-    createdAt: { type: Date, default: Date.now } // Default timestamp
-
-    
-});
-
-module.exports = mongoose.model('clients', ClientSchema);
+    gst: {
+      type: String, 
+      required: [true, 'Please enter gst'],
+    },
+    charge: {
+      type: String, 
+      required: [true, 'Please enter charge'],
+    },
+    createdAt: { type: Date, default: Date.now },
+  });
+  
