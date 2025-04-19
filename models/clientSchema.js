c859f153263e17c836dcc60037aa2710d46a0f8b
Test-codes-03 <ksudarshanksudarshan@gmail.com>
2025-04-19 22:44:31 +0530

    Update clientSchema.js

--- models/clientSchema.js
+++ models/clientSchema.js
@@ -1,44 +1,18 @@
-const mongoose = require('mongoose');
-const ClientSchema = mongoose.Schema({
-    userid:{
-        type: String,
-        required: [true, 'Please enter user id'],
-    },
-    name:{
-        type: String,
-        required: [true, 'Please enter proper name'],
-    },
-    amount:{
-        type: String,
-        required: [true, 'Please enter amount'],
-    },
-    email:{
-        type: String,
-        required: [true, 'Please enter email'],
-    },
-    phone_number:{
-        type: String,
-        required: [true, 'Please enter phone number'],
-    },
-    select_type:{
-        type: String,
-        required: [true, 'Please enter type'],
-    },
-     schemes:{
-        type: String,
-        required: [true, 'Please enter schemes'],
-    },
-    paymentId:{
-        type: String,
-        required: [true, 'Please enter paymentId'],
-    },
-    paymentStatus:{
-        type: String,
-        required: [true, 'Please enter paymentStatus'],
-    },
-    createdAt: { type: Date, default: Date.now } // Default timestamp
+// models/clientSchema.js
+const mongoose = require("mongoose");
 
-    
-});
+const clientSchema = new mongoose.Schema({
+  userid: String,
+  name: String,
+  amount: Number,
+  email: String,
+  phone_number: String,
+  select_type: String,
+  schemes: String,
+  gst: String,
+  charge: Number,
+  paymentId: String,
+  paymentStatus: String,
+}, { timestamps: true });
 
-module.exports = mongoose.model('clients', ClientSchema);
+module.exports = mongoose.model("Client", clientSchema);
